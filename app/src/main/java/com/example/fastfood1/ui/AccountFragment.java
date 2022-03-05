a67dcb4e05d7221abb683399be606a8c2d736bb4
Admin <mg222612@gmail.com>
2022-03-05 11:34:06 +0530

    fregment

---
+++ app/src/main/java/com/example/fastfood1/ui/AccountFragment.java
@@ -0,0 +1,57 @@
+package com.example.fastfood1.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.fastfood1.databinding.FragmentHomeBinding;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AccountFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+
+    private FragmentHomeBinding binding;
+
+    public static AccountFragment newInstance(int index) {
+        AccountFragment fragment = new AccountFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
